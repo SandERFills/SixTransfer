93843d2a5a5941e370feb1c49403f7f55746e845
SandERFills <salagaev98@mail.ru>
2021-05-18 13:02:02 +0300

    fist com

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xtransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transfer transfer = new Transfer();
+            Console.WriteLine(transfer.StringNum("98664"));
+       
+            
+        }
+    }
+}
---
+++ Transfer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xtransfer
+{
+    class Transfer
+    {
+        public int InputNum { get; set; }
+        public string StringNum(string num)
+        {
+            char[] stringArr=new char[num.Length] ;
+            string result=String.Empty;
+            int remains;
+            int buffInt = int.Parse(num);
+            for (int i = num.Length-1; buffInt>0; i--)
+            {
+
+            
+                remains = buffInt % 16;
+                buffInt /= 16;
+                if (remains==10)
+                {
+                    result = "a";
+                }
+                else if (remains == 11)
+                {
+                    result = "b";
+                }
+                else if (remains == 12)
+                {
+                    result = "c";
+                }
+                else if (remains == 13)
+                {
+                    result = "d";
+                }
+                else if (remains == 14)
+                {
+                    result = "e";
+                }
+                else if (remains == 15)
+                {
+                    result = "f";
+                }
+                else
+                {
+                    result = Convert.ToString(remains);
+                }
+                stringArr[i] = Convert.ToChar(result);
+            }
+            string str = new string(stringArr);
+            return str;
+        }
+    }
+}
